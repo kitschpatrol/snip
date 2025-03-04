ff958559bc73a6dd8f086911287cc3e4d88ebcf8
Eric Mika <eric@ericmika.com>
2025-03-04 01:47:18 -0500

    Update dependencies. Fix repository link.

--- eslint.config.ts
+++ eslint.config.ts
@@ -11,9 +11,6 @@ export default eslintConfig({
 				},
 			],
 			'import/no-named-as-default-member': 'off',
-			// Knip workaround...
-			// https://github.com/webpro-nl/knip/issues/158#issuecomment-1632648598
-			'jsdoc/check-tag-names': ['error', { definedTags: ['public'] }],
 		},
 	},
 })
